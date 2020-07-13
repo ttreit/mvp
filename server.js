3e5eb29ed11dc8971792c2d41487e3e025443863
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-13 15:23:29 -0700

    Add body-parser and initial get route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require('express');
+//requirements
+const express = require('express');  //express itself
+const bodyParser = require('body-parser'); //used for parsing incoming req bodies (node.js middlware)
 
+
+//create app and set port
 const app = express();
 const port = process.env.port || 1337;
 
+//static files available to server root
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+  res.sendFile(index.html);
+})
+
+//start server
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
+
+//TODO How to escape/prevent xss attacks
