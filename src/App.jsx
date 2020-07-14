ff5e55d36fd64d43ad377483db26b563fde57bdb
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-13 18:43:41 -0700

    Add constructor and initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1
+    }
+  }
   render() {
     return(
       <div className='App'>
