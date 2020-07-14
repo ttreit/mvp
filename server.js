490a82982f367378e0105c6b9bfde4d3a00e88d3
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-13 17:15:09 -0700

    Change default port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser'); //used for parsing incoming req bodie
 
 //create app and set port
 const app = express();
-const port = process.env.port || 1337;
+const port = process.env.port || 5347;
 
 //static files available to server root
 app.use(express.static('public'));
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 //routes
 app.get('/', (req, res) => {
   res.sendFile(index.html);
-})
+});
 
 //start server
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
