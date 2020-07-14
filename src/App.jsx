bfa2574c315d8c31398e823fdc00ada298dd5066
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-13 19:39:17 -0700

    Testing component integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SubmissionForm from './SubmissionForm';
 
 class App extends Component {
   constructor(props) {
@@ -9,8 +10,11 @@ class App extends Component {
   }
   render() {
     return(
-      <div className='App'>
+      <div className='heading'>
         <h1>Treit Family Homonym Tracker</h1>
+        <SubmissionForm />
+
+
       </div>
     );
   }
---
+++ src/SubmissionForm.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function SubmissionForm(props) {
+  return <h2>TEST SubmissionForm</h2>;
+}
+
+export default SubmissionForm;
