9c1c67c5ea842018280e39e2b99959c9dd017a76
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-15 16:40:34 -0700

    Style SubmissionForm

--- src/SubmissionForm.jsx
+++ src/SubmissionForm.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 function SubmissionForm(props) {
   return (
-    <form action='/homonyms' method='post'>
+    <form action='/' method='post'>
       <ul>
         <li>
           <label for='inputhomonyms'></label>
-          <textarea id='inputhomonyms' name='input_homonyms'>Enter one set of homonyms like this: hart, heart</textarea>
+          <textarea id='inputhomonyms' name='input_homonyms'>Example: hart, heart</textarea>
         </li>
         <li class='button'>
           <button type='submit'>Add Homonyms!</button>
