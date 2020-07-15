3e349e354d20e22e0bfc50e36e9a20535b1ed31e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-14 19:51:32 -0700

    Remove mongoose quick start code, add simple connection

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const express = require('express');  //express itself
 const bodyParser = require('body-parser'); //used for parsing incoming req bodies (node.js middlware)
 const mongoose = require('mongoose'); //makes connecting to mongoDB easier
 
-//Mongoose alias
-const db = mongoose.connection;
 
 //create app and set port
 const app = express();
 const port = process.env.port || 5347;
-const dataLocation = 'mongodb://localhost/'
+
 
 //static files available to server root
 app.use(express.static('public'));
@@ -20,33 +18,11 @@ app.get('/', (req, res) => {
 });
 
 //connect to mongoDb (Atlas)
-
-//mongodb+srv://Tammy:<GoHawks>@seahawk1.b4fyp.mongodb.net/<dbname>?retryWrites=true&w=majority
-mongoose.connect(dataLocation, {useNewUrlParser: true});
-//test mongoDB connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open',() => {
-  //log connection
-  console.log(`Connected to ${dataLocation}`);
-  //refer to schema and create properties
-  const homonymSchema = new mongoose.Schema({
-    set: Array,
-    user: String
-  });
-  //compile schema into a Model
-  //a Model is a class with which we construct documents
-  const HomonymSet = mongoose.model('HomonymSet', homonymSchema);
-  //TODO replace [heart, hart] hardcoded with set passed in to server from req.body
-  const inputSet = ['great', 'grate']; //TODO get this from req.body
-  const newSet = new HomonymSet({set: inputSet})
-  console.log(newSet.set);
-  //save record to DB
-  newSet.save((err, newSet) => {
-    if (err) return console.error(err);
-    newSet;
-  });
-
-})
+mongoose.connect(
+  'mongodb+srv://Tammy:GoHawks@seahawk1.b4fyp.mongodb.net/homonyms?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to Atlas')
+);
 
 
 //start server
