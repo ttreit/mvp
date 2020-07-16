dc6deb2c16d101d26a051092494341c3d6189a73
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-15 22:12:04 -0700

    Move SubmissionForm component into App
    Will refactor but this worked for now to get it rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
 import SubmissionForm from './SubmissionForm';
+import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       page: 1,
-      currentInput: ''
-    }
+      inputText: '',  //inputText
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({inputText: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    //AJAX
+    $.ajax({
+      type: 'POST',
+      url: '/',
+      data: this.state.inputText
+    })
+  }
+
   render() {
-    return(
-      <div className='heading'>
-        <h1>Homonym Tracker</h1>
-        <SubmissionForm />
-      </div>
-    );
-  }
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Homonyms:
+        <input type='string' value={this.state.inputText} onChange={this.handleChange} />
+      </label>
+      <input type='submit' value='Enter Homonyms!' />
+    </form>
+  );
+}
+
+
+
+  // render() {
+  //   return(
+  //     <div className='heading'>
+  //       <h1>Homonym Tracker</h1>
+  //       <SubmissionForm />
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
--- src/SubmissionForm.jsx
+++ src/SubmissionForm.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
 function SubmissionForm(props) {
-  return (
-    <form action='/' method='post' enctype='text/plain'>
-      <ul>
-        <li>
-          <label for='inputhomonyms'></label>
-          <textarea id='inputhomonyms' name='input_homonyms'>hart, heart</textarea>
-        </li>
-        <li class='button'>
-          <button type='submit'>Add Homonyms!</button>
-        </li>
-      </ul>
-    </form>
-  )
+
+
 }
 
+
 export default SubmissionForm;
+
+// render() {
+//   return (
+//     <form onSubmit={props.handleSubmit}>
+//       <label>
+//         Homonyms
+//         <input type='text' value={props.state.currentInput} onChange={props.handleChange} />
+//       </label>
+//       <input type='submit' value='Enter Homonyms!' />
+//     </form>
+//   );
+// }
