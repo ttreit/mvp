8d2dafbcea47307908bc59055080e671f360d34d
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-15 19:21:35 -0700

    Change default text in input field

--- src/SubmissionForm.jsx
+++ src/SubmissionForm.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 function SubmissionForm(props) {
   return (
-    <form action='/' method='post'>
+    <form action='/' method='post' enctype='text/plain'>
       <ul>
         <li>
           <label for='inputhomonyms'></label>
-          <textarea id='inputhomonyms' name='input_homonyms'>Example: hart, heart</textarea>
+          <textarea id='inputhomonyms' name='input_homonyms'>hart, heart</textarea>
         </li>
         <li class='button'>
           <button type='submit'>Add Homonyms!</button>
