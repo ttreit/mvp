fb12a0917536566d1fef79111b1c3e18b3240e8e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-16 13:23:56 -0700

    Change to send a string instead of object

--- middleware/processString.js
+++ middleware/processString.js
@@ -1,12 +1,7 @@
 const processString = function (req, res, next) {
   for (key in req.body) {
-    const createSet = function(key) {
-      return {
-        set: key
-      }
-    }
-    req.body.set = createSet(key);
-    console.log('set ', req.body.set);
+    req.body.set = key;
+    console.log('set ', req.body.set, 'typeof: ', (typeof req.body.set));
   }
 
 next();
