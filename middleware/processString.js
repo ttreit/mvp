122c9d1ebe27e212fac89f993829491e02a99fa8
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-16 18:48:02 -0700

    Remove console.log

--- middleware/processString.js
+++ middleware/processString.js
@@ -1,7 +1,6 @@
 const processString = function (req, res, next) {
   for (key in req.body) {
     req.body.set = key;
-    console.log('set ', req.body.set, 'typeof: ', (typeof req.body.set));
   }
 
 next();
