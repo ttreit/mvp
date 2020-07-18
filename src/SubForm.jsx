c5fa94c02e79716829b53dcee378ac0562737eff
ttreit <14933277+ttreit@users.noreply.github.com>
2020-07-17 17:23:18 -0700

    Remove testing code

--- src/SubForm.jsx
+++ src/SubForm.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 function SubForm(props) {
   return (
     <div>
-      <h1>Hello {} </h1>
-      {console.log(props.currentList)}
+
     </div>
   )
-
 }
 
 export default SubForm;
